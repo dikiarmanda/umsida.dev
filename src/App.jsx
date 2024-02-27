984b586aa257b50d5f8c0a361b326d9892ff83e8
Diki Armanda <diki.armada5@gmail.com>
2024-02-27 23:45:02 +0700

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useEffect } from "react";
 import "./App.css";
-import { themeChange } from 'theme-change'
+import { themeChange } from "theme-change";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 export default function App() {
   useEffect(() => {
-    themeChange(false)
-  }, [])
-  
+    themeChange(false);
+  }, []);
+
   return (
     <>
       <Navbar />
-      Tes
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+export default function Hero() {
+  return (
+    <div className="hero h-screen bg-base-200" id="hero">
+      <div className="hero-content flex-col w-full lg:justify-around lg:flex-row-reverse">
+        <div>
+          <img
+            src="/src/assets/umsidadev.png"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+        </div>
+        <div className="lg:text-start">
+          <h1 className="text-5xl font-bold">UMSIDA.dev</h1>
+          <p className="py-6">Belajar pemrograman mudah bukan. Bukan!</p>
+          <button className="btn btn-primary">Join Now</button>
+        </div>
+      </div>
+    </div>
+  );
+}
