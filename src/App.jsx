cfaa7f52bb79cc4c320bbaa6f4db583293525397
Diki Armanda <diki.armada5@gmail.com>
2024-02-28 03:44:37 +0700

    install router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import { useEffect } from "react";
-import { themeChange } from "theme-change";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Event from "./pages/Event";
 
 export default function App() {
-  useEffect(() => {
-    themeChange(false);
-  }, []);
-
   return (
-    <div className="container lg:max-w-7xl mx-auto">
-      <Navbar />
-      <Hero />
-      <About />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/event" element={<Event />} />
+      </Routes>
+    </Router>
   );
 }
