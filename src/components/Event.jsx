3480d369f69fa0217c35d4da1d6ce26cb292b5d1
Diki Armanda <diki.armada5@gmail.com>
2024-02-29 20:50:44 +0700

    add events object

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,3 +1,71 @@
 export default function Event() {
-  
+  const ourEvent = [
+    {
+      id: 1,
+      title: "NGOBARv1.0",
+      subTitle:
+        "Mengenal Google Drivenya programmer and version control introduction",
+      tgl: "26 Juli 2022",
+    },
+    {
+      id: 2,
+      title: "NGOBARv2.0",
+      subTitle: "Fundamental Web",
+      tgl: "4 Agustus 2022",
+    },
+    {
+      id: 3,
+      title: "NGOBARv3.0",
+      subTitle: "Web Design : Styling & Layoting with CSS3",
+      tgl: "3 September 2022",
+    },
+    {
+      id: 4,
+      title: "NGOBARv4.0",
+      subTitle: "Web Programming : Javascript for Advanced Developer",
+      tgl: "17 September 2022",
+    },
+    {
+      id: 5,
+      title: "NGOBARv5.0",
+      subTitle: "Mencari Kebenaran pada Sistem Digital",
+      tgl: "22 Oktober 2022",
+    },
+    {
+      id: 6,
+      title: "NGOBARv6.0",
+      subTitle: "JavaScript Beginner : Make Your Game",
+      tgl: "21 Januari 2023",
+    },
+    {
+      id: 7,
+      title: "NGEBARv1.0",
+      subTitle: "Microsoft Excel Advenced",
+      tgl: "17 Maret 2023",
+    },
+    {
+      id: 8,
+      title: "NGOBARv7.0",
+      subTitle: "Introduction React",
+      tgl: "27 Mei 2023",
+    },
+    {
+      id: 9,
+      title: "NGOBARv1.1",
+      subTitle: "Belajar Web Dari Nol",
+      tgl: "2 Desember 2023",
+    },
+    {
+      id: 10,
+      title: "NGOBARv8.0",
+      subTitle: "Membuat Aplikasi Android dengan React Native",
+      tgl: "27 Januari 2024",
+    },
+  ]
+
+  return (
+    <div>
+
+    </div>
+  )
 }
